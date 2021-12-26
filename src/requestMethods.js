fba8f0eae2c1f5da9c6aca8ed2cb66a0356577a0
Imran Hossain <ih060899@users.noreply.github.com>
2021-12-26 14:20:03 +0600

    change token

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://ih-shop-api.herokuapp.com/api/';
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
-    .currentUser.accessToken;
+const TOKEN =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxN2UzZGM2ZDM4NTYyMDE4MDhkZTUwMiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MDQxNjEwMCwiZXhwIjoxNjQwNjc1MzAwfQ.oYNypePOkFwh_NYe9vsCmqCC5Vt3i3cOW44cT8wfFgI';
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
